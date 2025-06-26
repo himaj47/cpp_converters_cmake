6336ef23d8861a464acb6c9719a0a62544d574f5
himaj47 <himajjoshi932@gmail.com>
2025-06-26 06:34:56 +0530

    added custom command that outputs the msg description file in the build

--- src/cmake/msg_gen.cmake
+++ src/cmake/msg_gen.cmake
@@ -1,9 +1,19 @@
 function(convert_to_ros_msg TARGET_NAME FILE)
     find_program(msg_generator_path stm_converter REQUIRED)
-    
+
+    get_filename_component(basename ${FILE} NAME_WE)
+    set(msg_description "${CMAKE_CURRENT_BINARY_DIR}/${basename}_desc.yaml")
+
+    add_custom_command(
+    OUTPUT ${msg_description}
+    COMMAND ${msg_generator_path} ${FILE} --out-description "${msg_description}" --package "${PROJECT_NAME}"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    DEPENDS ${FILE}
+    VERBATIM
+    COMMENT "Generating message description from ${FILE}"
+    )
+ 
     add_custom_target(${TARGET_NAME}
-        COMMAND ${msg_generator_path} ${FILE}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        COMMENT "generating msg file for - ${FILE} in location ${CMAKE_SOURCE_DIR}"
+        DEPENDS ${msg_description}
     )
 endfunction()
